030b78151e0bf4d59c97f7a313098d558f650008
Vale <com.steff.m@gmail.com>
2025-04-22 19:01:24 -0700

    Gráfico de dispersión

---
+++ notebooks/gdd.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+fig.show() # crear gráfico de dispersión
